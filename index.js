2d6b3ffb9da5936f5ad6f4434821058943d13dcb
Matthew Mueller <mattmuelle@gmail.com>
2014-08-26 14:53:45 -0700

    change name and cleanup

--- index.js
+++ index.js
@@ -2,12 +2,8 @@
  * Module Dependencies
  */
 
-var relative = require('path').relative;
-var Package = require('duo-package');
-var filedeps = require('file-deps');
 var fmt = require('util').format;
 var join = require('path').join;
-var main = require('duo-main');
 
 /**
  * Export `compat`
@@ -29,6 +25,7 @@ function compat(opts) {
   return function *(file, entry) {
     if ('css' != entry.type) return;
     if ('css' != file.type) return;
+
     var path = join(file.root, 'component.json');
     var dir = join(entry.root, 'components');
     var obj = json(path);
@@ -39,20 +36,6 @@ function compat(opts) {
       file.src = fmt('@import "%s@%s";\n%s', pkg, deps[pkg], file.src);
     }
   }
-}
-
-/**
- * Fetch the packages
- *
- * @param {Array} pkgs
- * @return {Array}
- */
-
-function fetch(pkgs) {
-  return pkgs
-    .map(function(pkg) {
-      return pkg.fetch();
-    });
 }
 
 /**
